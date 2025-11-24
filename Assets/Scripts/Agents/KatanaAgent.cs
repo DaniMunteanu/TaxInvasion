9a71eae0cbb2eebf60b4208578375e04ca5d9d92
Dani Munteanu <daniel.munteanu@student.upt.ro>
2025-11-24 05:10:28 +0200

    Set pixel-snapping and started enemy navigation system

--- Assets/Scripts/Agents/KatanaAgent.cs
+++ Assets/Scripts/Agents/KatanaAgent.cs
@@ -1,19 +1,25 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class KatanaAgent : MonoBehaviour
 {
     [SerializeField]
-    GameObject objective;
+    Transform target;
+    
+    NavMeshAgent agent;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(target.position);
     }
 }
